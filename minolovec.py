68c687b3173fd50540b4ec0993392630893e0594
Meta Trdin <meta.trdin5@gmail.com>
2020-08-01 17:58:52 +0200

    dodala mapo views in napisala tezavnosti igre

--- minolovec.py
+++ minolovec.py
@@ -1,6 +1,7 @@
 import bottle
 import model
 
+SECRET = "Ena skrivnost"
 
 minolovec = model.Minolovec()
 
@@ -12,22 +13,36 @@ def zacetna_stran():
 
 @bottle.post("/nova_igra/")
 def nova_igra():
+    tezavnost = bottle.request.forms.getunicode("tezavnost")
+    if tezavnost == "lahko":
+        velikost_polja = 8
+        st_min = 10
+    
+    if tezavnost == "srednje":
+        velikost_polja = 16
+        st_min = 40
+
+    else:
+        velikost_polja = 22
+        st_min = 99
+
     id_igre = minolovec.nova_igra(velikost_polja, st_min)
-    #se piskotek
+    bottle.response.set_cookie("id_igre", id_igre, path='/', secret=SECRET)
     bottle.redirect("/igra/")
 
 @bottle.get("/igra/")
 def pokazi_igro():
-    #id_igre = bottle.request.get_cookie("id_igre", secret=SECRET)
+    id_igre = bottle.request.get_cookie("id_igre", secret=SECRET)
     [igra, velikost_polja, st_min, stanje] = minolovec.igre[id_igre]
+    # igra = Polje()
 
-    return bottle.template("igra.html", igra=igra, stanje=stanje, id_igre=id_igre, ZMAGA=model.ZMAGA, PORAZ=model.PORAZ)
+    return bottle.template("igra.html", igra=igra, stanje=stanje, id_igre=id_igre, ZMAGA=model.ZMAGA, PORAZ=model.PORAZ, st_min=st_min, velikost_polja=velikost_polja)
 
 #ni se v redu
 @bottle.post("/igra/")
 def ugibaj():
-    #id_igre = bottle.request.get_cookie("id_igre", secret=SECRET)
-    crka = bottle.request.forms.getunicode("crka")
+    id_igre = bottle.request.get_cookie("id_igre", secret=SECRET)
+    poskus = bottle.request.forms.getunicode("poskus")
     #podatke poslane prek POST preberes iz request.forms iz igra.html
     #dobit moram vr, st, zastavico
 
